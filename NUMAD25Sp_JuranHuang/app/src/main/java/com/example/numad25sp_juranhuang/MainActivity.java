e02a66b6e7ada31dee6be000278c9f66be6a168c
JuranHuang4399 <1360765282qqqq@gmail.com>
2025-01-15 01:37:56 -0800

    Updated Assignment1 version 1

---
+++ NUMAD25Sp_JuranHuang/app/src/main/java/com/example/numad25sp_juranhuang/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.numad25sp_juranhuang;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Button aboutMe = findViewById(R.id.aboutMeButton);
+        aboutMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //used for debugging (logcat)
+                Log.d("MainActivity", "Button clicked");
+
+                LayoutInflater inflater = getLayoutInflater();
+                View layout = inflater.inflate(R.layout.toast_layout, null);
+
+                //used for debugging (logcat)
+                if (layout == null) {
+                    Log.d("MainActivity", "Toast layout inflation failed");
+                } else {
+                    Log.d("MainActivity", "Toast layout inflated successfully");
+                }
+
+                TextView toastTextView = layout.findViewById(R.id.toastText);
+                toastTextView.setText("Juran Huang\nhuang.jur@northeastern.edu");
+                Toast toast = new Toast(getApplicationContext());
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setView(layout);
+                toast.show();
+            }
+        });
+    }
+}
